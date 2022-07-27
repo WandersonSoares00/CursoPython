633d3a54aaf034309b538b34f41543aa2913f00f
WandersonSoares00 <wandersonsoares841@gmail.com>
2022-07-27 18:36:13 -0300

    Tabuada.py

---
+++ Mundo1/Tabuada.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+num = int(input('Informe um numero para ver sua tabuada: '))
+
+print('\n 1 x {} = {:2}\n 2 x {} = {:2}\n 3 x {} = {:2}\n 4 x {} = {:2}\n 5 x {} = {:2}' .format(num, num*1,num, num*2,num, num*3, num, num*4, num, num*5))
+print(' 6 x {} = {:2}\n 7 x {} = {:2}\n 8 x {} = {:2}\n 9 x {} = {:2}\n10 x {} = {:2}' .format(num, num*6, num, num*7, num, num*8, num, num*9, num, num*10))
