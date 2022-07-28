df5bf466e837899fbfb842420a228369fe784655
WandersonSoares00 <109666442+WandersonSoares00@users.noreply.github.com>
2022-07-28 09:05:22 -0300

    Tabuada simples

--- Mundo1/Tabuada.py
+++ Mundo1/Tabuada.py
@@ -1,4 +1,4 @@
-#Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+#Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
 num = int(input('Informe um numero para ver sua tabuada: '))
 
 print('\n 1 x {} = {:2}\n 2 x {} = {:2}\n 3 x {} = {:2}\n 4 x {} = {:2}\n 5 x {} = {:2}' .format(num, num*1,num, num*2,num, num*3, num, num*4, num, num*5))
